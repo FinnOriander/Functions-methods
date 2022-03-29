49afbe39cdd2d9fcc807c552800ad060d59b4b3d
FinnOriander <102714329@student.swin.edu.au>
2022-03-29 12:26:22 +1100

    second

---
+++ functions/2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Welcome
+{
+    class Program
+    {
+    public static void welcome(string pName) //Name output function
+    {
+        Console.WriteLine("Welcome friend " + pName );
+    }
+    public static void HaveAnice() 
+    {
+        Console.WriteLine("Have a nice day!");
+    }
+    public static void Main(string[] args) 
+    {
+        string str1;
+        Console.Write("Please Enter a name : ");
+        str1 = Console.ReadLine();
+        welcome(str1);
+        HaveAnice();
+ 
+        Console.ReadLine();
+        }
+    }
+}
