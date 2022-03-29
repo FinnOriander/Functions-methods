fcf969f6f335ab93046ea738182d6e8b41ea1f5f
FinnOriander <102714329@student.swin.edu.au>
2022-03-29 15:30:14 +1100

    fivedone

---
+++ functions/4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {	public static int SpaceCount(string str)
+	  {   
+		 int sstt=0;
+		 string str1;
+		 for (int i = 0;i < str.Length;i++)
+			{
+			  str1 = str.Substring(i,1);
+			  if (str1 == " ")
+					sstt++;
+			}
+		return sstt;
+	  }
+        public static void Main()
+	  {
+	     string str2;
+		 Console.Write("Test Data:\n");
+		 Console.Write("Please input a string : ");
+         str2 = Console.ReadLine();
+		 Console.WriteLine("\""+str2+"\""+" contains {0} spaces", SpaceCount(str2) );
+	  }  
+    }    
+}
+
--- functions/5/Program.cs
+++ functions/5/Program.cs
@@ -3,10 +3,30 @@
 namespace _5
 {
     class Program
-    {
+    {public static int Add(int[] arr)
+        {
+            int total = 0;
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                total += arr[i];
+            }
+            return total;
+        }
+ 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int sizeArray = 0;
+            Console.Write("Input the size of Array : ");
+            sizeArray = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[sizeArray];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("Number {0} : ", (i + 1));
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("The Sum of the elements of the array is:" + Add(numbers));
+            Console.ReadKey();
         }
     }
 }
